5e5be3b9d6e674323c7eab54f4e55ea3c292ce7d
AxelZanella <axelzanella93@gmail.com>
2021-01-18 20:42:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const newLocal = $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false,
+        },
+        600: {
+            items: 1,
+            nav: false,
+        },
+        1000: {
+            items: 1,
+            nav: false,
+        }
+    }
+});
